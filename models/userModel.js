43811c8c2df6e781bf64986e58ce33ac53d63d71
davidjeffrey058 <113363460+davidjeffrey058@users.noreply.github.com>
2024-05-20 18:15:37 +0000

    First phase of authentication completed

--- models/userModel.js
+++ models/userModel.js
@@ -28,18 +28,26 @@ userSchema.statics.signup = async function (fullname, email, password) {
     if (!validator.isStrongPassword(password)) throw Error('Password must contain a number, a special character, a capital letter, and must be 8 length long');
 
     const exists = await this.findOne({ email });
-
-    if (exists) {
-        throw Error('Email already exists');
-    }
+    if (exists) throw Error('Email already exists');
 
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
 
-    const user = await this.create({ fullname, email, password: hash });
+    return await this.create({ fullname, email, password: hash });
+}
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+
+    if (!email || !password) throw Error('All Fields are required');
+
+    const user = await this.findOne({ email });
+    if (!user) throw Error('Email does not exist');
+    const compare = await bcrypt.compare(password, user.password);
+    if (!compare) throw Error('Incorrect password for that email');
 
     return user;
 }
 
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);
